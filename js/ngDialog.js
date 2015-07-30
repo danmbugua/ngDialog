69f6505176849ecdc67b346732354e2d6a205b46
danmbugua <daniel.mbugua@savannahinformatics.comm>
2015-07-30 16:49:02 +0300

    Add ng-style in directive in ngDialog.js
    Will be used to style the position of the popover in relation to
    the mouse click.

--- js/ngDialog.js
+++ js/ngDialog.js
@@ -482,7 +482,7 @@
                                 template += '<div class="ngdialog-close"></div>';
                             }
 
-                            $dialog = $el('<div id="ngdialog' + localID + '" class="ngdialog"></div>');
+                            $dialog = $el('<div id="ngdialog' + localID + '" ng-style="position_dialog" class="ngdialog"></div>');
                             $dialog.html((options.overlay ?
                                 '<div class="ngdialog-overlay"></div><div class="ngdialog-content" role="document">' + template + '</div>' :
                                 '<div class="ngdialog-content" role="document">' + template + '</div>'));
